c950acbd0f62e7d84954ef1ad811e6df452883b4
Rahul Raj <rrstor524@gmail.com>
2026-02-27 17:43:16 +0530

    some change

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,14 @@ app.get('/',(req,res)=>{
 })
 
 
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 const startServer = async () => {
