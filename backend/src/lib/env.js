56ca9232a9f3fb7813058191e141f8f153f95cc7
Rahul Raj <rrstor524@gmail.com>
2026-02-27 11:22:41 +0530

    db and tools setup

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -6,5 +6,5 @@ dotenv.config();
 export const ENV = {
   PORT : process.env.PORT || 5000,
   DB_URL : process.env.DB_URL,
-
+  NODE_ENV : process.env.NODE_ENV, 
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import path from 'path';
 import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+const __dirname = path.resolve();
 
 
 app.get('/',(req,res)=>{
@@ -14,6 +16,19 @@ app.get('/',(req,res)=>{
 
 
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
+
+const startServer = async () => {
+    try{
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+          
+          console.log(`Server is running on port ${ENV.PORT}`);
+         
 });
+    }
+    catch(error){
+        console.error("Failed to start server:", error);
+    }
+};
+
+startServer();
